82c88a1c297b9dea352538f9735baee310bf64f3
Ubuntu <ubuntu@ip-10-71-152-78.ec2.internal>
2014-01-26 00:15:09 +0000

    updated capistrano deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'source code management'
+set :repo_url, 'git@example.com:callowaylc/gw-scm.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/home/ubuntu/Develop/gw-scm'
+set :user, 'ubuntu'
 
 # Default value for :scm is :git
 # set :scm, :git
