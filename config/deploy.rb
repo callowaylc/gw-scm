221e77153b8eb9f49815b72d45e3f3d2c0f7a374
Ubuntu <ubuntu@ip-10-71-152-78.ec2.internal>
2014-01-26 01:06:06 +0000

    more deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.1.0'
 
 set :application, 'gw-scm'
-set :repo_url, 'git@example.com:callowaylc/gw-scm.git'
+set :repo_url, 'git@github.com:callowaylc/gw-scm.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
